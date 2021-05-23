1a5d8fbe2ee35df84114197a679d92baa83a0afb
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-05-22 18:39:34 -0600

    Almost all tests passing.

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,28 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars').orderBy('id');
 }
 
-const getById = () => {
+const getById = (id) => {
   // DO YOUR MAGIC
+  return db('cars').where({id}).first();
 }
 
-const create = () => {
+const create = async (car) => {
   // DO YOUR MAGIC
+  const [id] = await db('cars').insert(car);
+  return db('cars').where({id}).first();
+}
+
+const getByVin = (vin) => {
+  return db('cars').where(vin=vin);
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  getByVin
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,40 @@
 // DO YOUR MAGIC
+const express = require('express');
+
+const router = express.Router();
+
+const {checkCarId, checkCarPayload, 
+checkVinNumberValid, checkVinNumberUnique} = require('./cars-middleware');
+
+const Cars = require('./cars-model');
+
+router.get('/api/cars', async (req, res) => {
+    try {
+        const cars = await Cars.getAll();
+        res.status(200).json(cars);
+    } catch(err) {
+        res.status(500).json({ message: "could not retrieve cars" });
+    }
+});
+
+router.get('/api/cars/:id', checkCarId, async (req, res) => {
+    const {id} = req.params;
+    try {
+        const car = await Cars.getById(id);
+        res.status(200).json(car);
+    } catch(err) {
+        res.status(500).json({ message: "could not retrieve car" });
+    }
+});
+
+router.post('/api/cars', checkVinNumberUnique, checkCarPayload, checkVinNumberValid, async (req, res) => {
+    const car = req.body;
+    try {
+        const newCar = await Cars.create(car);
+        res.status(201).json(newCar);
+    } catch(err) {
+        res.status(500).json({ message: "could not create new car" });
+    }
+});
+
+module.exports = router;
